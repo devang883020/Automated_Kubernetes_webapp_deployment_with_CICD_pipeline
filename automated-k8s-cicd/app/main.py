17a9c3b33715bb8059c5cdb2dc337eb72a02a9fc
devang883020 <devangkubde632@gmail.com>
2025-12-17 17:52:32 +0530

    version 2

--- automated-k8s-cicd/app/main.py
+++ automated-k8s-cicd/app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins CI/CD on EKS! with webhook gitlab and it is my first project and if there is any change please tell me "
+    return "Hello from Jenkins CI/CD on EKS! with webhook github and CI with github action "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
